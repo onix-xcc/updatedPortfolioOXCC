5ca62c49f19fdcbc95093a83eb9a132c6c474ee3
onix-xcc <onix.xcc@gmail.com>
2019-04-05 23:56:26 -0500

    Initial commit, had worked on all of it from my desktop, has a lot of room for improvement but I nailed the requirements down, I think...

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// Makes hamburger icon shift from burger to an x
+$(document).ready(function () {
+
+    $('.first-button').on('click', function () {
+  
+      $('.animated-icon1').toggleClass('open');
+    });
+    $('.second-button').on('click', function () {
+  
+      $('.animated-icon2').toggleClass('open');
+    });
+    $('.third-button').on('click', function () {
+  
+      $('.animated-icon3').toggleClass('open');
+    });
+  });
